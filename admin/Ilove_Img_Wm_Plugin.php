0bb2aa5f97ade4bb82f1d8e92a1ca103153455af
Diego Orellano <die.ore@Mac-mini.local>
2024-03-01 12:53:33 -0300

    update plugin to v2.0.3

--- admin/Ilove_Img_Wm_Plugin.php
+++ admin/Ilove_Img_Wm_Plugin.php
@@ -18,7 +18,7 @@ class Ilove_Img_Wm_Plugin {
 	 * @access   public
 	 * @var      string    VERSION    The current version of the plugin.
 	 */
-    const VERSION = '2.0.2';
+    const VERSION = '2.0.3';
 
     /**
 	 * The unique identifier of this plugin.
